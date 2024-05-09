f086a395755b8aadf5de23c7940d07a1e1539794
PQFitz <9113979@qq.com>
2024-05-09 16:35:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -189,7 +189,8 @@ download() {
 
 # get server ip
 get_ip() {
-    export "$(_wget -6 -qO- https://cloudflare.com/cdn-cgi/trace | grep ip=)" &>/dev/null
+    export "$(_wget -4 -qO- https://cloudflare.com/cdn-cgi/trace | grep ip=)" &>/dev/null
+    [[ -z $ip ]] && export "$(_wget -6 -qO- https://cloudflare.com/cdn-cgi/trace | grep ip=)" &>/dev/null
 }
 
 # check background tasks status
